d35e302be933180a84a8f29afb12872754577a10
Trofimov44 <73187280+Trofimov44@users.noreply.github.com>
2025-04-14 21:46:28 +0300

    Update main.py

--- andreii/main.py
+++ andreii/main.py
@@ -28,8 +28,8 @@ async def response(client, message):
         })
 
         # ⚠️ API-ключ OpenRouter
-        api_key = "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"  # начинающийся с "sk-or-"
-        api_key2 = "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"    
+        api_key = ""  # начинающийся с "sk-or-"
+        api_key2 = ""    
         
         headers = {
             "Authorization": f"Bearer {api_key2}",
